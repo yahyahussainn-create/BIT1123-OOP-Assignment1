b036b66769a19d4b2bae7e4e86571ee92e90695b
yahyahussainn-create <yahyahussainn@gmail.com>
2026-08-18 02:47:15 +0500

    Add files via upload

---
+++ StudentGrade.java
@@ -0,0 +1,9 @@
+public class StudentGrade {
+    public static void main(String[] args) {
+        String studentName = "Ali";
+        double mark = 75.0;
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Mark: " + mark);
+    }
+}
